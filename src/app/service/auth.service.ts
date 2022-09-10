be86c6fd423bdbe9cf2b9536b86b9c20a0cc6069
Merielly Santana <santana.meri10gmail.com>
2022-09-10 16:46:17 -0300

    Implementação do get e post de temas

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -15,7 +15,7 @@ export class AuthService {
   ) { }
 
   entrar(userLogin: UserLogin): Observable<UserLogin>{
-    return this.http.post<UserLogin>('http://localhost:8080/usarios/logar', userLogin)
+    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
   }
 
   cadastrar(user: User): Observable<User>{
